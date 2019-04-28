a9fd4ee68764fbc9df5e6925babd4c184e70ad19
aoizora <mathesis@inbox.ru>
2019-04-28 17:33:12 +0300

    Spring JPA REST

---
+++ Spring Boot Simple REST/src/main/java/aoizora/repository/CatRepository.java
@@ -0,0 +1,9 @@
+package aoizora.repository;
+
+import aoizora.domain.Cat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CatRepository extends JpaRepository<Cat, Long> {
+}
